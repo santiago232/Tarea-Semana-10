f98e556468c011b136eba3effa22911f019de483
fazt <wilson@faztweb.com>
2024-04-19 11:54:15 -0600

    Terminado

---
+++ Seccion10s/Seccion10s/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Seccion10s
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //Grid grid = new Grid();
+            ////Asignamos la grilla a la ventana actual.
+            //this.Content = grid;
+            //Button btn = new Button();
+            //btn.FontSize = 26;
+            //btn.Width = 150;
+            //btn.Height = 50;
+
+            //WrapPanel wrapPanel = new WrapPanel();
+            //TextBlock textBlock = new TextBlock();
+            //textBlock.Text = "Boton";
+            //textBlock.Foreground = Brushes.Blue;
+
+            //wrapPanel.Children.Add(textBlock);
+
+            //btn.Content = wrapPanel;
+            //grid.Children.Add(btn);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            MessageBox.Show("Has presionado el boton " + button.Name);
+        }
+
+        private void Button_ClickInterno(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Has dado clic en el interno");
+        }
+
+        private void Button_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Has dado clic sobre el boton externo");
+        }
+
+        private void Button_PreviewMouseDown_1(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Has dado clic sobre el boton interno");
+        }
+
+        private void Window_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Has dado clic sobre la ventana");
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Externo");
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Interno");
+        }
+
+
+    }
+}
---
+++ WinFormsApp/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Text = "WinFormsApp1";
+
+            // Define los controles de la interfaz aquí
+            this.Controls.Add(new System.Windows.Forms.TextBox()
+            {
+                Name = "txt1",
+                Width = 200,
+                Location = new System.Drawing.Point(50, 50),
+                BackColor = System.Drawing.Color.AliceBlue,
+                Text = "Hola Mundo"
+            });
+
+            this.Controls.Add(new System.Windows.Forms.ListBox()
+            {
+                Name = "listBox1",
+                Height = 100,
+                Width = 100,
+                Location = new System.Drawing.Point(50, 100),
+                Items = { "Item 1", "Item 2", "Item 3" }
+            });
+
+            this.Controls.Add(new System.Windows.Forms.Button()
+            {
+                Name = "button1",
+                Text = "Clic aquí",
+                Font = new System.Drawing.Font("Microsoft Sans Serif", 16F),
+                Height = 50,
+                Width = 150,
+                Location = new System.Drawing.Point(50, 210),
+            });
+
+            this.Controls.Add(new System.Windows.Forms.Label()
+            {
+                Name = "label1",
+                Text = "Label",
+                Location = new System.Drawing.Point(200, 300),
+            });
+        }
+    }
+}
